b147ae9735e536ab5dfe7f8ae137ebe9de5a6506
Ana María <developeranamaria@gmail.com>
2023-08-01 20:51:46 -0500

    Update Styles

--- src/components/headerCards/HeaderCards.jsx
+++ src/components/headerCards/HeaderCards.jsx
@@ -5,6 +5,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../movieCard/movieCard.scss';
+import './headerCards.scss'
 
 const HeaderCards = () => {
   const [movies, setMovies] = useState([]);
